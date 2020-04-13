6f135a0325479214d97d97532fab1823de36ab26
Arjun Sarin <arjun@sarin.de>
2020-04-13 14:45:17 +0200

    [fix] update snake to use base class Game [refactor] kill driver concept in favor of main.py

---
+++ main.py
@@ -0,0 +1,80 @@
+from time import sleep
+from threading import Thread
+import queue
+
+from src.telegram.telegram_bot import TelegramBot
+from src.games.menu import Menu
+from src.utils.output import Output
+
+
+def application(input_q, output):
+    # start() function returns if the game is over
+    # Then return from the application so that the thread can terminate 
+    menu = Menu(input_q, output)
+    menu.start()
+
+    output.emptyMatrix()
+
+# def inputs(input_q, b_pins=[7, 24]):
+#     """ Processes inputs by pushing them to the shared input_q
+#     """
+#     # Save the last time that each button was pressed for debouncing
+#     last_pressed = {}
+#     # Initialize all buttons provided in the list of pins
+#     for pin in b_pins:
+#         last_pressed[pin] = time.time()
+#         GPIO.setmode(GPIO.BCM)
+#         GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+#         GPIO.add_event_detect(pin, GPIO.RISING)
+#
+#     # Constantly check for Pressed buttons
+#     while True:
+#         if GPIO.input(b_pins[0]):
+#             if debounce(b_pins[0], last_pressed):
+#                 input_q.put_nowait('right')
+#                 print('right')
+#         elif GPIO.input(b_pins[1]):
+#             if debounce(b_pins[1], last_pressed):
+#                 input_q.put_nowait('left')
+#                 print('left')
+#         sleep(0.05)
+#
+#
+# def debounce(pin, last_pressed):
+#     """ Provides button debouncing
+#     """
+#     # Checks the last time that button has been pressed
+#     last_time = last_pressed[pin]
+#     # If time difference is reasonable, we assume that it is valid bress of a button
+#     if abs(time.time() - last_time) > 0.2:
+#         last_pressed[pin] = time.time()
+#         return True
+#     # If time difference is too small, the button is just bouncing
+#     else:
+#         return False
+
+
+def telegram_inputs(input_q):
+    bot = TelegramBot(input_q)
+    bot.run()
+
+
+# Create output class to pass to all threads
+output = Output()
+
+input_q = queue.Queue(maxsize=3)
+input_q.put('Running')
+
+# This is for hardware switches only
+#input_thread = Thread(target=inputs, args=(input_q,), daemon=True)
+#input_thread.start()
+
+telegram_thread = Thread(target=telegram_inputs, args=(input_q,), daemon=True)
+telegram_thread.start()
+
+app_thread = Thread(target=application, args=(input_q, output))
+app_thread.start()
+
+app_thread.join()
+
+print("Done")
--- src/games/snake.py
+++ src/games/snake.py
@@ -1,36 +1,31 @@
-from output import Output
 import time
-import copy
+from src.games.Game import Game
 from random import randint
 from time import sleep
 
-class Garden():
-    """ This class implements the game snake.
-    
-    24.03.2019
-    """
-    def __init__(self, input_q, world_dim=(12, 12)):
-        # Initialize directions and pass input queue
+class Snake(Game):
+    # Define snake, fruit and background color
+    snake_color = (5, 255, 5)
+    fruit_color = (255, 0, 0)
+    head_color = (10, 255, 255)
+    garden_color = (0, 0, 0)
+
+    def __init__(self, input_q, output):
+        super().__init__(input_q, output)
+
+        # Initialize directions
         self.compass = ["north", "east", "south", "west"]
-        self.direction = randint(1,3)
-        self.input_q = input_q
+        self.direction = randint(1, 3)
 
         # Snake is a list of coordinates
-        self.snake = [(5,5), (5, 6), (5,7)]
+        self.snake = [(5, 5), (5, 6), (5, 7)]
         # Fruit coordinate
-        self.fruit = (randint(5, 11), randint(7,11))
-        
+        self.fruit = (randint(5, 11), randint(7, 11))
+
         # Checks if game is still running. Thread is terminated if False.
         self.active = True
-        
-        # Determine snake, fruit and background color
-        self.snake_color = (5, 255, 5)
-        self.fruit_color = (255, 0, 0)
-        self.head_color = (10, 255, 255)
-        self.garden_color = (0, 0, 0)
 
         # Initialize display elements
-        self.output = Output(world_dim[0], world_dim[1])
         self.update_pixelMatrix()
         self.print_world()
 
@@ -41,11 +36,11 @@ class Garden():
         if self.snake[0] in self.snake[1:]:
             print("Game over")
             self.active = False
-        
+
         else:
             # Check if fruit was eaten
-            if self.snake[0] == self.fruit:     
-            # Place new fruit, but not where the snake currently is
+            if self.snake[0] == self.fruit:
+                # Place new fruit, but not where the snake currently is
                 while True:
                     self.fruit = (randint(0, 11), randint(0, 11))
                     if self.fruit not in self.snake:
@@ -55,18 +50,18 @@ class Garden():
                 self.snake.pop()
             # Check for direction and place head accordingly
             if self.compass[self.direction] == "east":
-                 x_new = self.snake[0][0]+1
-                 y_new = self.snake[0][1]
+                x_new = self.snake[0][0] + 1
+                y_new = self.snake[0][1]
             elif self.compass[self.direction] == "west":
-                x_new = self.snake[0][0]-1
+                x_new = self.snake[0][0] - 1
                 y_new = self.snake[0][1]
             elif self.compass[self.direction] == "north":
                 x_new = self.snake[0][0]
-                y_new = self.snake[0][1]+1
+                y_new = self.snake[0][1] + 1
             elif self.compass[self.direction] == "south":
                 x_new = self.snake[0][0]
-                y_new = self.snake[0][1]-1
-            self.snake = [(x_new%12, y_new%12)] + self.snake
+                y_new = self.snake[0][1] - 1
+            self.snake = [(x_new % 12, y_new % 12)] + self.snake
 
     def update_pixelMatrix(self):
         """Prepares the matrix, displaying the snake and the fruit 
@@ -81,17 +76,17 @@ class Garden():
         i = 0
         for s in self.snake:
             self.output.pixelMatrix[s[0]][s[1]] = self.snake_color
-            #if length(self.snake) > 5:
-            #i = i+1
-            #self.output.pixelMatrix[s[0]][s[1]] = (self.snake_color[0], self.snake_color[1]-i*20, self.snake_color[2])
+            # if length(self.snake) > 5:
+            # i = i+1
+            # self.output.pixelMatrix[s[0]][s[1]] = (self.snake_color[0], self.snake_color[1]-i*20, self.snake_color[2])
         # Draw snake head
         self.output.pixelMatrix[self.snake[0][0]][self.snake[0][1]] = self.head_color
-        
+
     def print_world(self):
         """ Prints the world matrix to the table
         """
-        self.output.showRGB()
-        
+        self.output.show()
+
     def update_direction(self):
         """ Reacts to commands in the input queue and updates the direction accordingly
         """
@@ -104,20 +99,13 @@ class Garden():
                 self.direction = (self.direction - 1) % 4
             elif input == 'left':
                 self.direction = (self.direction + 1) % 4
- 
-    def clean_world(self):
-        """ Fill the whole matrix with zeros
-        """
-        for x, row in enumerate(self.output.pixelMatrix):
-            for y, col in enumerate(self.output.pixelMatrix):
-                self.output.pixelMatrix[x][y] = (0,0,0)
- 
+
     def game_over_animation(self):
         """ Animation to show when the game is over
         """
         last_pixelMatrix = self.output.pixelMatrix
         for i in range(4):
-            self.clean_world()
+            self.output.emptyMatrix()
             self.print_world()
             sleep(0.3)
             self.output.pixelMatrix = last_pixelMatrix
@@ -128,7 +116,7 @@ class Garden():
     def start(self):
         """ Runs the game
         """
-        while self.active==True:
+        while self.active:
             self.update_direction()
             self.move_snake()
             self.update_pixelMatrix()
@@ -137,3 +125,27 @@ class Garden():
         self.game_over_animation()
         return False
 
+    # TODO make static but how to access colors then?
+    def draw_icon(self, output):
+        """
+        Draws a dummy fruit and a dummy snake as icon on a given output.
+        """
+        super().draw_icon(output)
+
+        # dummy fruit
+        output.setValueRGB(3, 4, self.fruit_color)
+        # dummy snake
+        output.setValueRGB(7, 2, self.snake_color)
+        output.setValueRGB(7, 3, self.snake_color)
+        output.setValueRGB(7, 4, self.snake_color)
+        output.setValueRGB(7, 5, self.snake_color)
+        output.setValueRGB(8, 5, self.snake_color)
+        output.setValueRGB(8, 6, self.snake_color)
+        output.setValueRGB(8, 7, self.snake_color)
+        output.setValueRGB(8, 8, self.snake_color)
+        output.setValueRGB(7, 8, self.snake_color)
+        output.setValueRGB(6, 8, self.snake_color)
+        output.setValueRGB(5, 8, self.snake_color)
+        output.setValueRGB(4, 8, self.snake_color)
+
+        output.show()
