87a1282eac91702501cfa308b126598826e05fcb
Arjun Sarin <arjun@sarin.de>
2020-04-17 19:04:01 +0200

    additional implementations

--- table/games/Tetris.py
+++ table/games/Tetris.py
@@ -1,21 +1,51 @@
 from table.games.Game import Game
 import random
 import numpy as np
+from time import sleep
+
 
 class Tetris(Game):
+    # Background color as RGB tuple
+    background_color = (0, 0, 0)
+
     def __init__(self, postman, output):
         super().__init__(postman, output)
+        # Todo maybe create a new pixel Matrix and pass to output. Depending on outcome of issue #18
+        self.pixel_matrix = output.pixel_matrix
+
+        self.running = True
 
     def start(self):
+        while self.running:
+            speed = .8
+            current_block = Tetromino(self, (0, self.output.columns//2-1))
 
+            while current_block.fall():
+                sleep(speed)
+
+            # Block has landed
+            if not current_block.dissolve():
+                # Block couldn't dissolve
+                self.game_over_animation()
+
+            self.remove_full_rows()
+
+        # The Game has ended!
+        return True
+
+    def remove_full_rows(self):
+        raise NotImplemented
+
+    def game_over_animation(self):
+        raise NotImplemented
 
     def draw_icon(self, output):
         raise NotImplemented
 
 
-class Tetromino():
+class Tetromino:
     """ Class representing one Tetris Block"""
-    # Unrotated list of coordinates for different block shapes
+    # List of initial coordinates for different block shapes
     # The coordinates are relative to the block's origin
     # Tuples are arranged in (row, col)
     shapes = {
@@ -31,6 +61,8 @@ class Tetromino():
     def __init__(self, GameInstance: Tetris, origin: tuple):
         self.game = GameInstance
         self.origin = origin
+        # Todo maybe create a new pixel Matrix and pass to output. Depending on outcome of issue #18
+        self.pixel_matrix = GameInstance.output.pixel_matrix
 
         # Select shape and color randomly
         random.seed()
@@ -45,20 +77,21 @@ class Tetromino():
 
     def _check_validity(self, intended_origin, intended_pixels):
         # Check validity for each pixel one by one
-        for pixel in intended_pixels:
-            row = intended_origin(0)+pixel(0)
-            col = intended_origin(1)+pixel(1)
+        for intended_pixel in intended_pixels:
+            row = intended_origin(0)+intended_pixel(0)
+            col = intended_origin(1)+intended_pixel(1)
 
             if (row < 0) or (row > self.occupancy_matrix.shape[0]):
                 return False
             if (col < 0) or (col > self.occupancy_matrix.shape[1]):
                 return False
-            if self.occupancy_matrix[row, col]:
-                return False
+            if (self.pixel_matrix[row, col] != self.game.background_color) and \
+                (intended_pixel not in self.pixels):
+                    # This pixel is already occupied by the other fallen blocks
+                    return False
 
         # Intended position passed all tests
         return True
-
 
     def rotate(self):
         """ Rotates block if possible.
@@ -70,7 +103,7 @@ class Tetromino():
 
         # Rotate each pixel one by one in clockwise direction
         for pixel in self.pixels:
-            rotated_pixel = (pixel(0), -pixel(1))
+            rotated_pixel = (pixel[0], -pixel[1])
             new_pixels.add(rotated_pixel)
 
         if self._check_validity(self.origin, new_pixels):
@@ -85,7 +118,7 @@ class Tetromino():
         Returns:
             bool:   False if moving is not possible, e.g. blocks would hit wall or other block.
         """
-        new_origin = (self.origin(0), self.origin(1)-1)
+        new_origin = (self.origin[0], self.origin[1]-1)
 
         if self._check_validity(new_origin, self.pixels):
             self.origin = new_origin
@@ -99,7 +132,7 @@ class Tetromino():
         Returns:
             bool:   False if moving is not possible, e.g. blocks would hit wall or other block.
         """
-        new_origin = (self.origin(0), self.origin(1)+1)
+        new_origin = (self.origin[0], self.origin[1]+1)
 
         if self._check_validity(new_origin, self.pixels):
             self.origin = new_origin
@@ -111,9 +144,9 @@ class Tetromino():
         """ Moves block one field down if possible.
 
         Returns:
-            bool:   False if falling is not possible, e.g. blocks would ground wall or other block.
+            bool:   False, if falling is not possible, e.g. blocks would ground wall or other block.
         """
-        new_origin = (self.origin(0)+1, self.origin(1))
+        new_origin = (self.origin[0]+1, self.origin[1])
 
         if self._check_validity(new_origin, self.pixels):
             self.origin = new_origin
@@ -121,10 +154,23 @@ class Tetromino():
         else:
             return False
 
-    def render(self):
-        """"
-        Adds this block to the pixelMatrix. Note: Does not call Output.show()
+    def dissolve(self):
+        """ Blocks is transformed to permanent pixel.
+
+        Is called when falling isn't possible anymore. Therefore the block is added to the
+        pixel_matrix as permament pixel and the class instance is destructed.
+
+        Returns:
+            bool: False, if dissolving would end in invalid move, e.g. game is over.
         """
         for pixel in self.pixels:
+            # Todo When issue #18 is dissolved this should modify pixel_matrix instead of output
+            self.game.output.set_value_color(pixel(0), pixel(1), self.color)
+
+    def render(self):
+        """ Adds this block to the pixelMatrix. Note: Does not call Output.show()
+        """
+        for pixel in self.pixels:
+            # Todo When issue #18 is dissolved this should modify pixel_matrix instead of output
             self.game.output.set_value_color(pixel(0), pixel(1), self.color)
 
