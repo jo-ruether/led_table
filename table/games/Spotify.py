505861827d83e79ea82ab85093109c5a3bde0be4
Arjun Sarin <arjun@sarin.de>
2020-05-06 23:36:06 +0200

    [Spotify] feature: control playback state

--- table/games/Spotify.py
+++ table/games/Spotify.py
@@ -6,6 +6,7 @@ import numpy as np
 
 from table.games.Game import Game
 from table.Postman import Topics
+from table.utils.Commands import CMD
 
 import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -17,9 +18,10 @@ class Spotify(Game):
     def __init__(self, postman, output, config_handler):
         super().__init__(postman, output)
 
+        self.config_handler = config_handler
         self.client_id = config_handler.get_value("Spotify", "client_id")
         self.client_secret = config_handler.get_value("Spotify", "client_secret")
-        self.scope = 'user-read-playback-state'
+        self.scope = 'user-modify-playback-state user-read-playback-state'
         self.redirect_uri = "https://www.google.de"
         self.spotifyObject = None
 
@@ -31,8 +33,10 @@ class Spotify(Game):
         self.postman.send(Topics.OUTPUT, "I am connected to Spotify now. Let's listen to some "
                                          "awesome tunes. 🎵")
 
-        while True:
+        self.running = True
+        while self.running:
             self.print_cover()
+            self.read_user_input()
 
     def connect_to_spotify(self) -> spotipy.Spotify:
         """ Connects to spotiy and prompts user for authorization if necessary.
@@ -40,14 +44,23 @@ class Spotify(Game):
             Returns:
                 Authorized Spotify object to access spotify API.
         """
-        # Request username
-        self.postman.send(Topics.OUTPUT, "Please send me your Spotify user name.")
+        username = self.config_handler.get_value("Spotify", "username")
 
-        # Wait until username is received
-        post = None
-        while not post:
-            post = self.postman.request(Topics.INPUT)
-        username = post['message']
+        if username:
+            self.postman.send(Topics.OUTPUT, f"I am using the last used Spotify username ({username}).")
+            #TODO "To connect to another account send `account` "
+
+        else:
+            # Request username
+            self.postman.send(Topics.OUTPUT, "Please send me your Spotify user name.")
+
+            # Wait until username is received
+            post = None
+            while not post:
+                post = self.postman.request(Topics.INPUT)
+            username = post['message']
+
+            self.config_handler.set_value("Spotify", "username", username)
 
         cache_path = ".cache-" + username
         sp_oauth = spotipy.SpotifyOAuth(
@@ -99,6 +112,28 @@ class Spotify(Game):
 
         self.output.pixel_matrix = np.array(img)
         self.output.show()
+
+    def read_user_input(self):
+        post = self.postman.request(Topics.INPUT)
+        while post:
+            cmd = post['message']
+            if cmd == CMD.RIGHT:
+                self.spotifyObject.next_track()
+            elif cmd == CMD.LEFT:
+                self.spotifyObject.previous_track()
+            elif cmd == CMD.X:
+                current_track = self.spotifyObject.current_user_playing_track()
+                if current_track["is_playing"]:
+                    self.spotifyObject.pause_playback()
+                else:
+                    self.spotifyObject.start_playback()
+            elif cmd == CMD.START:
+                self.running = False
+                break
+
+            # Check if there is even more to read
+            post = self.postman.request(Topics.INPUT)
+
 
     def draw_icon(self, output):
         super().draw_icon(output)
